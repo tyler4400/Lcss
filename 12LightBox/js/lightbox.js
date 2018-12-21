0182dd0b3ef6a3daea516f62c60963a9f95ced96
tyler4400 <tyler4400@foxmail.com>
2018-12-21 17:28:25 +0800

    3-3 同组别数据获取

--- 12LightBox/js/lightbox.js
+++ 12LightBox/js/lightbox.js
@@ -3,12 +3,28 @@
  */
 (function ($) {
     let LightBox = function () {
+        let  _this = this;
 
         this.popupMask = $(`<div id="G-lightbox-mask"></div>`);
         this.popupWin = $(`<div id="G-lightbox-popup"></div>`);
         this.bodyNode = $(document.body);
 
-        this.renderDOM();
+        // this.renderDOM();
+
+        this.groupName = null;
+        this.groupData = [];
+
+        this.bodyNode.delegate("div,.js-lightbox,*[data-role=lightbox]", "click", function (e) {
+            let curGroupName = $(this).attr('data-group');
+            if(curGroupName !== _this.groupName){
+                _this.groupName = curGroupName;
+                _this.getGroupData();
+                console.log(_this.groupData);
+            }
+
+            //停止事件冒泡
+            e.stopPropagation();
+        })
     };
 
     LightBox.prototype = {
@@ -29,6 +45,18 @@
                 `;
             this.popupWin.html(dom);
             this.bodyNode.append(this.popupMask, this.popupWin);
+        },
+        getGroupData () {
+            let _this = this;
+            _this.groupData = [];
+            let groupList = this.bodyNode.find(`*[data-group=${this.groupName}]`);
+                groupList.each(function () {
+                    _this.groupData.push({
+                        src: $(this).attr('data-source'),
+                        id: $(this).attr('data-id'),
+                        caption: $(this).attr('data-caption')
+                    });
+                });
         }
     };
 
