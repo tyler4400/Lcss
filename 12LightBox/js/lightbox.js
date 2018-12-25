c86bae27eb1daea8e8fc44ef016e6d17af7b3502
tyler4400 <tyler4400@foxmail.com>
2018-12-25 10:32:42 +0800

    3-4 初始化弹出框

--- 12LightBox/js/lightbox.js
+++ 12LightBox/js/lightbox.js
@@ -3,36 +3,92 @@
  */
 (function ($) {
     let LightBox = function () {
-        let  _this = this;
+        let _this = this;
 
-        this.popupMask = $(`<div id="G-lightbox-mask"></div>`);
-        this.popupWin = $(`<div id="G-lightbox-popup"></div>`);
+        this.popupMask = $(`<div id="G-lightbox-mask" class="d-n"></div>`);
+        this.popupWin = $(`<div id="G-lightbox-popup" class="d-n"></div>`);
         this.bodyNode = $(document.body);
-
-        // this.renderDOM();
+        this.JDOM = null;
 
         this.groupName = null;
         this.groupData = [];
 
-        this.bodyNode.delegate("div,.js-lightbox,*[data-role=lightbox]", "click", function (e) {
+        this.renderDOM();
+        this.JDOM = this.getJDOM();
+
+        this.bodyNode.delegate(".js-lightbox,*[data-role=lightbox]", "click", function (e) {
+            //停止事件冒泡
+            e.stopPropagation();
+
+            //获取group信息
             let curGroupName = $(this).attr('data-group');
-            if(curGroupName !== _this.groupName){
+            if(_this.groupName !== curGroupName) {
                 _this.groupName = curGroupName;
-                _this.getGroupData();
+                _this.groupData = _this.getGroupData();
                 console.log(_this.groupData);
             }
 
-            //停止事件冒泡
-            e.stopPropagation();
-        })
+            //初始化弹出
+            _this.initPopup($(this));
+
+        });
+
+
     };
 
     LightBox.prototype = {
+        initPopup (JDOM) {
+            let _this = this;
+            let curObj = {
+                sourceSrc : JDOM.attr('data-source'),
+                currentId : JDOM.attr('data-id'),
+            };
+            _this.showMaskAndPopup(curObj);
+
+        },
+        showMaskAndPopup (obj) {
+            //隐藏图片和caption区域
+            let _this = this;
+            _this.JDOM.popupPic.hide();
+            _this.JDOM.picCaptionArea.hide();
+
+            //遮罩 fade in (mask和win事先是display:none的)
+            _this.popupMask.fadeIn();
+
+            //设置合适的picViewArea大小
+            let winW = $(window).width(),
+                winH = $(window).height();
+            _this.JDOM.picViewArea.css({width: winW / 2, height: winH / 2});
+
+            //展现弹出窗口
+            _this.popupWin.fadeIn();
+            _this.popupWin.css({
+                width: winW / 2 + 10,
+                height: winH / 2 + 10,
+                marginLeft: -(winW / 2 + 10) / 2,
+                top: -(winH / 2 + 10) ,
+            }).animate({top: (winH / 2 + 10) / 2}, 'fast', function () {
+                console.log('加载图片');
+            });
+
+        },
+        getJDOM () {
+            return {
+                picViewArea: this.popupWin.find('div.lightbox-pic-view'),
+                popupPic: this.popupWin.find('img.lightbox-img'),
+                picCaptionArea: this.popupWin.find('div.lightbox-pic-caption'),
+                nextBtn: this.popupWin.find('span.lightbox-next-btn'),
+                prevBtn: this.popupWin.find('span.lightbox-prev-btn'),
+                captionText: this.popupWin.find('p.lightbox-pic-des'),
+                currentIndex: this.popupWin.find('span.lightbox-of-index'),
+                closeBtn: this.popupWin.find('span.lightbox-close-btn'),
+            };
+        },
         renderDOM () {
             let dom = `
                 <div class="lightbox-pic-view">
                     <span class="lightbox-btn lightbox-prev-btn lightbox-prev-btn-show"></span>
-                    <img src="img/wallhaven-716979.jpg" width="100%" alt="" class="lightbox-image">
+                    <!--<img src="img/wallhaven-716979.jpg" width="100%" alt="" class="lightbox-image">-->
                     <span class="lightbox-btn lightbox-next-btn lightbox-next-btn-show"></span>
                 </div>
                 <div class="lightbox-pic-caption">
@@ -47,16 +103,16 @@
             this.bodyNode.append(this.popupMask, this.popupWin);
         },
         getGroupData () {
-            let _this = this;
-            _this.groupData = [];
+            let groupData = [];
             let groupList = this.bodyNode.find(`*[data-group=${this.groupName}]`);
-                groupList.each(function () {
-                    _this.groupData.push({
-                        src: $(this).attr('data-source'),
-                        id: $(this).attr('data-id'),
-                        caption: $(this).attr('data-caption')
-                    });
+            groupList.each(function () {
+                groupData.push({
+                    src: $(this).attr('data-source'),
+                    id: $(this).attr('data-id'),
+                    caption: $(this).attr('data-caption')
                 });
+            });
+            return groupData;
         }
     };
 
